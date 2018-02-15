8a2e28e9e99952692e8c2cc78abf7ce5b074c368
Laurent P. René de Cotret <LaurentRDC@users.noreply.github.com>
2018-02-15 17:57:16 -0500

    Tweaks

--- uedinst/gatan.py
+++ uedinst/gatan.py
@@ -24,7 +24,10 @@ class GatanUltrascan895(AbstractContextManager):
 
     def __init__(self, addr = "127.0.0.1", port = 42057, **kwargs):
         self._socket = socket.socket()
-        self._socket.connect((addr, port))
+        try:
+            self._socket.connect((addr, port))
+        except ConnectionRefusedError:
+            raise InstrumentException('Could not connect to Digital Micrograph. Make sure it is open.')
     
     def __exit__(self, *args, **kwargs):
         self._socket.close()
@@ -64,16 +67,14 @@ class GatanUltrascan895(AbstractContextManager):
         toggle = str(toggle).upper()
         self.send_command('ULTRASCAN;INSERT;', toggle)
     
-    def acquire_image(self, exposure, antiblooming = True):
+    def acquire_image(self, exposure):
         """ 
-        Acquire a gain-normalized image.
+        Acquire a fully-processed image.
         
         Parameters
         ----------
         exposure : float
             Exposure [seconds].
-        antiblooming : bool, optional
-            If True (default), perform antiblooming.
         
         Returns
         -------
@@ -83,15 +84,34 @@ class GatanUltrascan895(AbstractContextManager):
         ------
         InstrumentException : if answer received indicates an error occurred.
         """
-        exposure = float(exposure)
-        antiblooming = str(antiblooming).upper()
         # Use a temporary file so that there can never be any conflits
         # between subsequent acquisitions. The resulting image is read before the file is deleted.
         # Note: we cannot use NamedTemporaryFile because it doesn't create
         # a name, but a file-like object.
         temp_filename = join(gettempdir(), "_uedinst_temp.tif")
-        self.send_command("ULTRASCAN;ACQUIRE;{:.3f},{},{}".format(exposure, temp_filename, antiblooming))
+        self.acquire_image_to_file(temp_filename, exposure)
         image = imread(temp_filename)
-        remove(temp_filename)
+        return image.mean(axis = 2)
+    
+    def acquire_image_to_file(self, filename, exposure):
+        """ 
+        Acquire a fully-processed image and save directly to file.
         
-        return image
+        Parameters
+        ----------
+        filename : str or path-like
+            Absolute path to a file.
+        exposure : float
+            Exposure [seconds].
+
+        Raises
+        ------
+        InstrumentException : if answer received indicates an error occurred.
+
+        See Also
+        --------
+        acquire_image : acquire an image as a NumPy array
+        """
+        exposure = float(exposure)
+        filename = str(filename)
+        self.send_command("ULTRASCAN;ACQUIRE;{:.3f},{}".format(exposure, filename))
