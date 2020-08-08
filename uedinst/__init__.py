756065cc93fe6b74b67ffc0e6b086f3747b22b1d
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-08-08 10:28:15 -0400

    Version bump

--- uedinst/__init__.py
+++ uedinst/__init__.py
@@ -2,7 +2,7 @@
 __author__ = "Laurent P. René de Cotret"
 __email__ = "laurent.renedecotret@mail.mcgill.ca"
 __license__ = "Proprietary"
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 
 class InstrumentException(Exception):
