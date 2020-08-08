990a65f3f375fabd6018fd487dfff2d71d089927
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-08-08 10:27:29 -0400

    Added variable attentuator motor driver

--- uedinst/attenuator.py
+++ uedinst/attenuator.py
@@ -51,3 +51,140 @@ class VariableAttenuator(AbstractContextManager):
         result = self.lib.get_move_settings(self.device_id, ct.byref(mvst))
         self.errcheck(result)
         return mvst.Speed
+
+    def get_position(self):
+        x_pos = pyximc.get_position_t()
+        result = self.lib.get_position(self.device_id, ct.byref(x_pos))
+        self.errcheck(result)
+        return x_pos.Position, x_pos.uPosition
+    
+    def left(self):
+        result = self.lib.command_left(self.device_id)
+        self.errcheck(result)
+
+    
+    def move(self, distance, udistance):
+        result = self.lib.command_move(self.device_id, distance, udistance)
+        self.errcheck(result)
+
+
+    def set_speed(self, speed):
+        # Create move settings structure
+        mvst = pyximc.move_settings_t()
+        # Get current move settings from controller
+        result = self.lib.get_move_settings(self.device_id, ct.byref(mvst))
+        self.errcheck(result)
+        # Print command return status. It will be 0 if all is OK
+        # Change current speed
+        mvst.Speed = int(speed)
+        # Write new move settings to controller
+        result = self.lib.set_move_settings(self.device_id, ct.byref(mvst))
+        # Print command return status. It will be 0 if all is OK
+        self.errcheck(result)
+
+    def status(self):
+        x_status = pyximc.status_t()
+        result = self.lib.get_status(self.device_id, ct.byref(x_status))
+        if result == Result.Ok:
+            print("Status.Ipwr: " + repr(x_status.Ipwr))
+            print("Status.Upwr: " + repr(x_status.Upwr))
+            print("Status.Iusb: " + repr(x_status.Iusb))
+            print("Status.Flags: " + repr(hex(x_status.Flags)))
+        self.errcheck(result)
+
+    def wait_for_stop(self,interval):
+        result = self.lib.command_wait_for_stop(self.device_id, interval)
+        self.errcheck(result)
+
+    
+    def set_microstep_mode_256(self):
+        print("\nSet microstep mode to 256")
+        # Create engine settings structure
+        eng = pyximc.engine_settings_t()
+        # Get current engine settings from controller
+        result = self.lib.get_engine_settings(self.device_id, ct.byref(eng))
+        # Print command return status. It will be 0 if all is OK
+        print("Read command result: " + repr(result))
+        # Change MicrostepMode parameter to MICROSTEP_MODE_FRAC_256
+        # (use MICROSTEP_MODE_FRAC_128, MICROSTEP_MODE_FRAC_64 ... for other microstep modes)
+        eng.MicrostepMode = pyximc.MicrostepMode.MICROSTEP_MODE_FRAC_256
+        # Write new engine settings to controller
+        result = self.lib.set_engine_settings(self.device_id, ct.byref(eng))
+        # Print command return status. It will be 0 if all is OK
+        self.errcheck(result)
+
+    def set_microstep_mode_FULL(self):
+        print("\nSet microstep mode to 256")
+        # Create engine settings structure
+        eng = pyximc.engine_settings_t()
+        # Get current engine settings from controller
+        result = self.lib.get_engine_settings(self.device_id, ct.byref(eng))
+        # Print command return status. It will be 0 if all is OK
+        print("Read command result: " + repr(result))
+        # Change MicrostepMode parameter to MICROSTEP_MODE_Full
+        # (use MICROSTEP_MODE_FRAC_128, MICROSTEP_MODE_FRAC_64 ... for other microstep modes)
+        eng.MicrostepMode = pyximc.MicrostepMode.MICROSTEP_MODE_FULL
+        # Write new engine settings to controller
+        result = self.lib.set_engine_settings(self.device_id, ct.byref(eng))
+        # Print command return status. It will be 0 if all is OK
+        self.errcheck(result)
+
+    def stop(self):
+        result = self.lib.command_stop(self.device_id)
+        self.errcheck(result)
+
+    def right(self):
+        result = self.lib.command_right(self.device_id)
+        self.errcheck(result)
+
+    def info(self):
+        print("\nGet device info")
+        x_device_information = pyximc.device_information_t()
+        result = self.lib.get_device_information(self.device_id, ct.byref(x_device_information))
+        print("Result: " + repr(result))
+        if result == Result.Ok:
+            print("Device information:")
+            print(" Manufacturer: " +
+                    repr(pyximc.string_at(x_device_information.Manufacturer).decode()))
+            print(" ManufacturerId: " +
+                    repr(pyximc.string_at(x_device_information.ManufacturerId).decode()))
+            print(" ProductDescription: " +
+                    repr(pyximc.string_at(x_device_information.ProductDescription).decode()))
+            print(" Major: " + repr(x_device_information.Major))
+            print(" Minor: " + repr(x_device_information.Minor))
+            print(" Release: " + repr(x_device_information.Release))
+
+    def set_zero(self):
+        result = self.lib.command_zero(self.device_id)
+        self.errcheck(result)
+
+    def soft_stop(self):
+        result = self.lib.command_sstp(self.device_id)
+        self.errcheck(result)
+
+    def save_settings(self):
+        """save from controllers ram"""
+        result = self.lib.command_save_settings(self.device_id)
+        self.errcheck(result)
+
+    def load_settings(self):
+        """"write to controllers ram"""
+        result = self.lib.command_read_settings(self.device_id)
+        self.errcheck(result)
+
+    def home_zero(self):
+        result = self.lib.command_homezero(self.device_id)
+        self.errcheck(result)
+
+    def home(self):
+        result = self.lib.command_home(self.device_id)
+        self.errcheck(result)
+        
+    def get_home_settings(self):
+        result = self.lib.get_home_settings(self.device_id)
+        self.errcheck(result)
+
+    @property
+    def get_max_speed(self):
+        mtr = pyximc.control_settings_t()
+        print(f'Max speed: {ct.byref(mtr.MaxSpeed)}')
