38f085b9b41a70f89af51c83156c1c0dfc031fd6
Laurenz Kremeyer <laurenz.kremeyer@mail.mcgill.ca>
2022-06-09 15:44:01 -0400

    added new freq_counter TTi TF930

--- uedinst/__init__.py
+++ uedinst/__init__.py
@@ -22,7 +22,7 @@ from .attenuator import VariableAttenuator
 from .daq import PCI6281
 from .delay_stage import ILS250PP
 from .electrometer import Keithley6514
-from .freq_counter import RacalDana1991
+from .freq_counter import RacalDana1991, TTiTF930
 from .gatan import GatanUltrascan895, GatanUltrascan895WithElectrometer
 from .multimeter import TekDMM4040
 from .merlin import Merlin
